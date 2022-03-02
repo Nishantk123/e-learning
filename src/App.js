9e0e4f4d4a816af06cd5ca92cd4203c4282014ac
Nishant kashyap <sannukashyap24@gmail.com>
2022-03-02 08:24:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './component/Home';
+import Enquiry from './component/Enquiry';
+import Header from './component/Header';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+     <Route exact path="/" component={Home} />
+     <Route exact path="/enquiry" component={Enquiry} />
     </div>
+    </Router>
   );
 }
 
---
+++ src/component/Enquiry.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SideNav from "./SideNav";
+
+const Enquiry = () =>{
+    return(
+        <div className="d-flex">
+            <SideNav />
+            <div className="w-100 p-3">
+                second section
+            </div>
+        </div>
+    )
+}
+
+export default Enquiry;
