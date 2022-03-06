7d5fd77f7ccf74c96d5b8837a37d24a154a15a2a
Nishant kashyap <sannukashyap24@gmail.com>
2022-03-06 17:23:27 +0530

    register

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './component/Home';
 import Enquiry from './component/Enquiry';
 import Header from './component/Header';
-
-
+import Register from './component/Register';
+import Enrollment from './component/Enrollement';
 function App() {
   return (
     <Router>
@@ -13,6 +13,8 @@ function App() {
       <Header />
      <Route exact path="/" component={Home} />
      <Route exact path="/enquiry" component={Enquiry} />
+     <Route exact path="/register" component={ Register} />
+     <Route exact path="/enrollment" component={Enrollment} />
     </div>
     </Router>
   );
---
+++ src/component/Enrollement.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Enrollment = () =>{
+
+    return(
+        <div>
+            This is Enrollment page
+        </div>
+    )
+}
+
+export default Enrollment;
